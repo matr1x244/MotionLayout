e9e7c04e857271d31487e57caf12996bc23583e7
matr1x244 <zio-21@yandex.ru>
2022-12-16 22:01:00 +0300

    Финальный макет при включении происходит анимация, без нажатия на элемент

--- app/src/main/java/com/example/motionlayout/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/motionlayout/ui/main/MainFragment.kt
@@ -1,13 +1,17 @@
 package com.example.motionlayout.ui.main
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.renderscript.Allocation
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.motionlayout.databinding.FragmentMainBinding
-import com.google.android.material.snackbar.Snackbar
 
 class MainFragment : Fragment() {
 
@@ -30,6 +34,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         startMotion()
+        binding.mainFragment.transitionToStart()
     }
 
     private fun startMotion() {
